e14e05433b94c828ad20fa281ef9fae054346773
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-08-05 14:45:25 +0900

    Add GameCli component
    GameCli 컴포넌트 추가

---
+++ components/GameCli.js
@@ -0,0 +1,16 @@
+export default function GameCli() {
+  
+  return (
+    <div className="container">
+      <div className="title">
+        <div className="buttons">
+          <div className="close">&times;</div>
+          <div className="minimum">-</div>
+          <div className="maximum">□</div>
+        </div>
+      </div>
+      <div className="display-wrapper">
+      </div>
+    </div>
+  );
+};
