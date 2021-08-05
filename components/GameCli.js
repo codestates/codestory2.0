96cca2a0e2be38a2fa010711d0d949ee4099535a
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-08-05 19:10:09 +0900

    Add GameCli component
    GameCli 컴포넌트 추가
    CSS 파일 작성

--- components/GameCli.js
+++ components/GameCli.js
@@ -1,16 +1,90 @@
-export default function GameCli() {
-  
+import { useState, useEffect } from 'react';
+import styles from './gamecli.module.css';
+import axios from 'axios';
+
+export default function GameCli({ stage, handleStageChange, isWaiting, handleWaiting, wd, handleWdChange, handleFinish }) {
+
+  const serverUrl = 'https://api.codestory.academy';
+  const [cli, setCli] = useState([`Last login: ${new Date().toUTCString()}`]);
+  const [command, setCommand] = useState('');
+  const [enterCount, setEnterCount] = useState(0);
+  const [isPassword, setIsPassword] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios.post(serverUrl+'/game/answer', { stage, command }, { withCredentials: true });
+      if (result.data.result) {
+        handleStageChange(result.data.script, true);
+        const commandArr = command.match(/\S+/g) || [];
+        switch (commandArr[0]) {
+        case 'cd':
+          handleWdChange(commandArr[1].match(/([^/]+)$/)[1]); break;
+        case 'ls':
+          setCli([...cli, '.password']); break;
+        case 'cat':
+          setCli([...cli, 'password: 1234', 'path: ~/Desktop/.hidden']); break;
+        case 'sudo':
+          setIsPassword(true); break;
+        }
+      }
+      else {
+        if (isPassword) {
+          handleStageChange(null, false);
+        }
+        setCli([...cli, isPassword ? 'ERROR: Permission denied' : `command not found: ${command}`]);
+      }
+      setCommand('');
+      handleWaiting();
+      if (isPassword) {
+        setIsPassword(false);
+      }
+    })();
+  }, [enterCount]);
+
+  const inputText = (e) => {
+    setCommand((isPassword ? command : '') + e.target.value);
+  };
+
+  const onKeyPress = (e) => {
+    if(e.charCode === 13) {
+      if(stage === '8') {
+        handleFinish();
+      }
+      setCli([...cli, isPassword ? 'Password:' : `${wd} $ ${command}`]);
+      handleWaiting();
+      setEnterCount(enterCount + 1);
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="title">
-        <div className="buttons">
-          <div className="close">&times;</div>
-          <div className="minimum">-</div>
-          <div className="maximum">□</div>
+    <div className={styles.container}>
+      <div className={styles.titlebar}>
+        <div className={styles.buttons}>
+          <div className={styles.close}>&times;</div>
+          <div className={styles.minimum}>-</div>
+          <div className={styles.maximum}>□</div>
         </div>
       </div>
-      <div className="display-wrapper">
+      <div className={styles.display-wrapper}>
+        <div className={styles.display}>
+          {cli.map((line, i) => <div key={i}>{line}</div>)}
+          {isWaiting
+            ? <div>잠시 기다려주세요</div>
+            : <div className={styles.input-wrapper}>
+              {isPassword ? 'Password:' : `${wd} $`}
+              <input
+                className={styles.input-commands}
+                type="text"
+                placeholder={isPassword ? '' : '명령어를 입력하세요'}
+                value={isPassword ? '' : command}
+                onChange={inputText}
+                onKeyPress={onKeyPress}
+                autoFocus
+              />
+            </div>}
+        </div>
       </div>
+      <div className={styles.scroll}></div>
     </div>
   );
 };
