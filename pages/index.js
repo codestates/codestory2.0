ab3a1017c504ef8d40f62620f423a140f846107f
seoyong-lee <sy3783@gmail.com>
2021-08-06 11:03:02 +0900

    Migrate to Sass
    Sass 적용

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import Nav from '../components/Nav';
