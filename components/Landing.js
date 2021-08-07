bbfbd79459c16b8e4ed53b4e1f0ce0988ec438b5
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-08-08 01:15:07 +0900

    Add Landing component CSS
    Landing 컴포넌트 CSS 추가
    Footer 컴포넌트 CSS 추가

--- components/Landing.js
+++ components/Landing.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './landing.module.css';
+import Image from 'next/image';
+import Nav from './Nav';
+import Footer from './Footer';
 
 export default function Landing() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.header}>
+        <Image
+          className={styles.logo}
+          src="/logotype.svg"
+          alt="Logo type"
+          width={115}
+          height={90}
+        />
+        <Nav />
+      </div>
+      <Footer />
     </div>
   );
 }
