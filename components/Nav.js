5fcae3383701d00e9ca570a7657eaa67acccf4b2
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-08-08 14:34:08 +0900

    Add Nav component CSS
    Nav 컴포넌트 CSS 작성
    모달 버튼 컴포넌트 구성

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
+import styles from './nav.module.css';
+import Image from 'next/image';
 
 export default function Nav() {
 
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const openHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.container}>
-    </div> 
+    <>
+      {!isOpen
+        ? <div className={styles.background_close}>
+          <Image
+            className={styles.nav_img}
+            src="/menu.svg"
+            alt="Nav"
+            width={65}
+            height={65}
+            onClick={openHandler}
+          />
+        </div>
+        : <div className={styles.background_open}>
+          <div className={styles.container}>
+            <button className={styles.btn_close} onClick={openHandler}>&times;</button>
+            <div className={styles.menu}>
+              <button className={styles.btn_about}>About</button>
+              <button className={styles.btn_game}>Game</button>
+              <button className={styles.btn_ranking}>Ranking</button>
+              <button className={styles.btn_mypage}>My page</button>
+              <button className={styles.btn_login}>Login</button>
+            </div>
+            <div className={styles.space}></div>
+          </div> 
+        </div>  
+      }
+    </>
   );
 }
