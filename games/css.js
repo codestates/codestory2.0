499e5ab91e273c6a3932076041cd049a93103bdc
ruleBased848 <rkdfowns@naver.com>
2021-08-10 08:04:23 +0900

    Update css game

--- games/css.js
+++ games/css.js
@@ -1,46 +1,87 @@
-const config = {
-  type: Phaser.AUTO,
-  scale: {
-    parent: 'game',
-    mode: Phaser.Scale.FIT,
-    width: 720,
-    height: 672.8
-  },
-  backgroundColor: '#E0E0E0',
-  dom: { createContainer: true },
-  scene: { create }
-};
-const game = new Phaser.Game(config);
-
-// assets
-const div = document.createElement('div');
-div.textContent = '<div>';
-
-const textarea = document.createElement('textarea');
-textarea.style = `
+(() => {
+  const container = document.querySelector('#game');
+  const canvas = document.createElement('canvas');
+  container.append(canvas);
+  canvas.style = `
 all: unset;
+background-color: #E0E0E0;
+`;
+  canvas.width = 720;
+  canvas.height = 672.8;
+
+  // assets
+  const div = document.createElement('div');
+  div.textContent = 'Alive';
+  const divStyle = `
+position: absolute;
+left: 20px;
+top: 20px;
+transition: all 4s;
+width: 32.52px;
+height: 27px;
+`;
+  div.style = divStyle;
+
+  const textarea = document.createElement('textarea');
+  textarea.style = `
+all: unset;
+position: absolute;
+left: 535px;
+top: 6.4px;
 width: 180px;
 height: 660px;
 background-color: white;
 font: 16px 'Source Code Pro', monospace;
 `;
 
-const button = document.createElement('button');
-button.textContent = 'Apply';
+  const button = document.createElement('button');
+  button.textContent = 'Apply';
+  button.style = `
+all: unset;
+position: absolute;
+left: 606.235px;
+top: 636.5px;
+background-color: #E0E0E0;
+`;
+  button.addEventListener('click', () => div.style = `${divStyle}${textarea.value}`);
 
-// states
-let css = '';
+  // use assets
+  container.append(div, textarea, button);
 
-function create() {
-  const styledDiv = this.add.dom(0, 0, div);
-  styledDiv.setOrigin(0);
-  styledDiv.setDepth(-1);
+  // draw
+  const getDots = (points, interval) => {
+    const dots = [];
+    for (let i = 0; i < points.length; ++i) {
+      const [xi, yi] = points[i];
+      const [xf, yf] = points[(i + 1) % points.length];
+      const length = ((xf - xi) ** 2 + (yf - yi) ** 2) ** .5;
+      const [vx, vy] = [interval / length * (xf - xi), interval / length * (yf - yi)];
+      const num = Math.ceil(length / interval);
+      dots.push(...new Array(num).fill(0).map((e, i) => [xi + i * vx, yi + i * vy]));
+    }
+    return dots;
+  };
+  const points = [[10, 10], [520, 10], [520, 660], [10, 660], [10, 440], [265, 440], [265, 230], [10, 230]];
+  const interval = 1;
+  const dots = getDots(points, interval);
+  const ctx = canvas.getContext('2d');
+  for (let dot of dots) {
+    ctx.beginPath();
+    ctx.rect(dot[0] - 0.5, dot[1] - 0.5, 1, 1);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+  }
 
-  const cssEditor = this.add.dom(625, 336.4, textarea);
-  cssEditor.addListener('keyup');
-  cssEditor.on('keyup', (e) => css = e.target.value);
-
-  const apply = this.add.dom(625, 650, button);
-  apply.addListener('click');
-  apply.on('click', () => styledDiv.setElement(div, css));
-}
+  // detect collision
+  const left = Number(container.style.left.match(/\d+/)[0]);
+  const top = Number(container.style.top.match(/\d+/)[0]);
+  const collisionHandler = () => {
+    for (let dot of dots) {
+      if (document.elementFromPoint(left + dot[0], top + dot[1]) === div) {
+        div.textContent = 'Dead';
+      }
+    }
+  };
+  setInterval(collisionHandler, 10);
+})();
