8510c0370430d65ac91506d7b19cf54afb8f22f3
seoyong-lee <sy3783@gmail.com>
2021-08-11 00:14:53 +0900

    Add landing slider
    랜딩페이지 슬라이더 구현

--- components/Footer.js
+++ components/Footer.js
@@ -1,30 +1,34 @@
 import styles from '../styles/modules/footer.module.scss';
 
-export default function Footer() {
+export default function Footer({ isWhite }) {
   
   return (
-    <div className={styles.container}>
+    <div className={isWhite ? styles.container_white : styles.container}>
       <div className={styles.box}>
         <a href="https://github.com/codestates/codestory2.0">
-          <span className={styles.repo}>
+          <span className={isWhite ? styles.repo_white : styles.repo}>
             Github Repository
           </span>
         </a>
       </div>
       <div className={styles.box}>
-        <span className={styles.word}>
+        <span className={isWhite ? styles.word_white : styles.word}>
           © Copyright 2021 Codestory All Rights Reserved
         </span>
       </div>
       <div className={styles.box}>
-        <span className={styles.name}>
-          <a href="https://github.com/satoshi25">조경일(FE)</a>
-          <span className={styles.division}>|</span>
-          <a href="https://github.com/seoyong-lee">이서용(FE)</a>
-          <span className={styles.division}>|</span>
-          <a href="https://github.com/Candyroom136">김정빈(BE)</a>
-          <span className={styles.division}>|</span>
-          <a href="https://github.com/ruleBased848">강래준(BE)</a> 
+        <span className={styles.name_box}>
+          <a className={isWhite ? styles.name_white : styles.name} 
+            href="https://github.com/satoshi25">조경일(FE)</a>
+          <span className={isWhite ? styles.division_white : styles.division}>|</span>
+          <a className={isWhite ? styles.name_white : styles.name} 
+            href="https://github.com/seoyong-lee">이서용(FE)</a>
+          <span className={isWhite ? styles.division_white : styles.division}>|</span>
+          <a className={isWhite ? styles.name_white : styles.name} 
+            href="https://github.com/Candyroom136">김정빈(BE)</a>
+          <span className={isWhite ? styles.division_white : styles.division}>|</span>
+          <a className={isWhite ? styles.name_white : styles.name} 
+            href="https://github.com/ruleBased848">강래준(BE)</a> 
         </span>
       </div>
     </div>
