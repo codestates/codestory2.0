442b0b8067260d65d3e46e23a3b3d6d2bea76b65
Candyroom136 <candyroom136@naver.com>
2021-08-11 15:57:22 +0900

    initialize amplify and modify code for convention

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import '../styles/main.scss';
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
+import { Amplify, withSSRContext } from 'aws-amplify';
+import awsExports from '../src/aws-exports';
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
