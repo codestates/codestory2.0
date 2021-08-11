78d5c2682c02fade680538f548337747d1eb23bb
Candyroom136 <candyroom136@naver.com>
2021-08-11 16:47:50 +0900

    Add deploy feature
    배포를 위해 aws-amplify, amplify-cli를 설치
    app_js에 ssr true 구문 삽입
    linux.js, api/css.js에서 정적 파일을 불러올 때 axios로 호출하도록 수정

--- pages/api/css.js
+++ pages/api/css.js
@@ -1,9 +1,9 @@
-// const fs = require('fs'); // local 확인 시 활성
-const axios = require('axios'); // local 확인 시 주석
+const fs = require('fs'); // local 확인 시 활성
+// const axios = require('axios'); // local 확인 시 주석
 
 export default async function css(req, res) {
-  // const data = fs.readFileSync('games/css/main.js'); // local 확인 시 활성
-  const cssRes = await axios.get('https://feat-amplify-deploy.drl7u5zwhcbcc.amplifyapp.com/games/css/main.js'); // local 확인 시 주석
+  const data = fs.readFileSync('games/css/main.js'); // local 확인 시 활성
+  // const cssRes = await axios.get('https://feat-amplify-deploy.drl7u5zwhcbcc.amplifyapp.com/games/css/main.js'); // local 확인 시 주석
   const data = cssRes.data; // local 확인 시 주석
   res.status(200).json(data);
 }
