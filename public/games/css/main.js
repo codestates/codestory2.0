a4fa9513484d2196ae5f3579195bea0b4ad1c5f5
JEONGBIN KIM <55874843+Candyroom136@users.noreply.github.com>
2021-08-12 00:49:27 +0900

    Update main.js

--- public/games/css/main.js
+++ public/games/css/main.js
@@ -1,137 +1,177 @@
 (() => {
-  const container = document.querySelector('#game_container');
   const canvas = document.createElement('canvas');
-  container.append(canvas);
-  canvas.style = `
-all: unset;
-background-color: #E0E0E0;
-`;
-  canvas.width = 720;
-  canvas.height = 672.8;
-
-  // assets
-  const div = document.createElement('div');
-  div.textContent = '😐';
-  const divStyle = `
-position: absolute;
-left: 20px;
-top: 20px;
-transition: all 4s;
-width: 30px;
-height: 54px;
-background-color: pink;
-font-size: 30px;
-`;
-  div.style = divStyle;
-  let isReturning = false;
-
-  const textarea = document.createElement('textarea');
-  textarea.style = `
-all: unset;
-position: absolute;
-left: 535px;
-top: 6.4px;
-width: 180px;
-height: 660px;
-background-color: white;
-font: 16px 'Source Code Pro', monospace;
-`;
-
-  const button = document.createElement('button');
-  button.textContent = 'Apply';
-  button.style = `
-all: unset;
-position: absolute;
-left: 606.235px;
-top: 636.5px;
-background-color: #E0E0E0;
-`;
-  button.addEventListener('click', () => {
-    if (!isReturning) {
-      div.style = `${divStyle}${textarea.value}`
-    }
-  });
-
-  // use assets
-  container.append(div, textarea, button);
-
-  // draw
-  const getDots = (points, interval) => {
-    const dots = [];
-    for (let i = 0; i < points.length; ++i) {
-      const [xi, yi] = points[i];
-      const [xf, yf] = points[(i + 1) % points.length];
-      const length = ((xf - xi) ** 2 + (yf - yi) ** 2) ** .5;
-      const [vx, vy] = [interval / length * (xf - xi), interval / length * (yf - yi)];
-      const num = Math.ceil(length / interval);
-      dots.push(...new Array(num).fill(0).map((e, i) => [xi + i * vx, yi + i * vy]));
-    }
-    return dots;
-  };
-  // boundary
-  const points = [
-    [10, 10],
-    [520, 10],
-    [520, 660],
-    [10, 660],
-    [10, 440],
-    [265, 440],
-    [265, 370],
-    [125, 370],
-    [90, 405],
-    [55, 370],
-    [195, 230],
-    [230, 265],
-    [195, 300],
-    [265, 300],
-    [265, 230],
-    [10, 230]
-  ];
-  const interval = 4;
-  const dots = getDots(points, interval);
+  canvas.style = 'all: unset;';
+  const gameContainer = document.querySelector('#game_container');
+  gameContainer.append(canvas);
   const ctx = canvas.getContext('2d');
-  for (let dot of dots) {
+  const homeDir = { name: '~', children: { '.': null, Desktop: null } };
+  const desktop = { name: 'Desktop', children: { '.': null, '..': homeDir } };
+  homeDir.children['.'] = homeDir;
+  homeDir.children.Desktop = desktop;
+  desktop.children['.'] = desktop;
+  let wd = desktop;
+  let leftfolder = ['Recent', 'Desktop', 'Document', 'Download'];
+  const firstFolder = new Image();
+  const secondFolder = new Image();
+  firstFolder.src = 'folder_icon.png';
+  secondFolder.src = 'folder_icon.png';
+  const textArr = [`Last login: ${new Date().toUTCString()}`, `${wd.name} $ `];
+  const lengthLimit = 43;
+  const lineLimit = 10;
+  const fontSize = 16;
+  document.addEventListener('keydown', keyDownHandler);
+  function keyDownHandler(e) {
+    switch (e.keyCode) {
+    case 8:
+      if (textArr[textArr.length - 1].length > wd.name.length + 3) {
+        textArr[textArr.length - 1] = textArr[textArr.length - 1].slice(0, -1);
+      } break;
+    case 13:
+      const commandArr = textArr[textArr.length - 1].slice(wd.name.length + 3).match(/\S+/g) || [];
+      switch (commandArr[0]) {
+      case undefined:
+        break;
+      case 'cd':
+        if (!commandArr[1] || commandArr[1] === '~') {
+          wd = homeDir;
+        } else {
+          const path = commandArr[1].replace(/\/$/g, '');
+          const pathArr = path.split('/');
+          let newWd = wd;
+          let isDirectoryChanged = true;
+          for (let folder of pathArr) {
+            if (!newWd.children[folder] || !newWd.children[folder].children) {
+              textArr.push(`cd: no such file or directory: ${commandArr[1]}`);
+              isDirectoryChanged = false;
+              break;
+            } else {
+              newWd = newWd.children[folder];
+            }
+          }
+          if (isDirectoryChanged) {
+            wd = newWd;
+          }
+        } break;
+      case 'mkdir':
+        for (let i = 1; i < commandArr.length; ++i) {
+          if (Object.keys(wd.children).includes(commandArr[i])) {
+            textArr.push(`mkdir: ${commandArr[i]}: File exists`);
+          } else {
+            const newDir = { name: commandArr[i], children: { '.': null, '..': wd } };
+            newDir.children['.'] = newDir;
+            wd.children[commandArr[i]] = newDir;
+          }
+        } break;
+      case 'rm':
+        const options = [];
+        for (let i = 1; i < commandArr.length; ++i) {
+          if (commandArr[i][0] === '-' && commandArr[i].length > 1) {
+            options.push(...commandArr[i].slice(1).split(''));
+          }
+        }
+        if (options.includes('-')) {
+          textArr.push('rm: illegal option -- -');
+          break;
+        }
+        for (let i = 1; i < commandArr.length; ++i) {
+          if (commandArr[i][0] !== '-' || commandArr[i].length === 1) {
+            if (!Object.keys(wd.children).includes(commandArr[i])) {
+              textArr.push(`rm: ${commandArr[i]}: No such file or directry`);
+            } else if (wd.children[commandArr[i]].children && !options.includes('r')) {
+              textArr.push(`rm: ${commandArr[i]}: is a directory`);
+            } else if (commandArr[i] === '.' || commandArr[i] === '..') {
+
+            } else {
+              delete wd.children[`${commandArr[i]}`];
+            }
+          }
+        } break;
+      default:
+        textArr.push(`bash: command not found: ${commandArr[0]}`);
+      }
+      textArr.push(`${wd.name} $ `); break;
+    case 16:
+    case 20:
+      break;
+    default:
+      textArr[textArr.length - 1] += e.key === 'Unidentified' ? '' : e.key;
+    }
+  }
+  function drawBackGround() {
     ctx.beginPath();
-    ctx.rect(dot[0] - 0.5, dot[1] - 0.5, 1, 1);
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#eee';
+    ctx.fill();
+    ctx.font = `${fontSize}px Arial`;
     ctx.fillStyle = 'black';
+    ctx.fillText(leftfolder[0], 0.05 * canvas.width, 0.12 * canvas.height);
+    ctx.fillText(leftfolder[1], 0.05 * canvas.width, 0.2 * canvas.height);
+    ctx.fillText(leftfolder[2], 0.05 * canvas.width, 0.28 * canvas.height);
+    ctx.fillText(leftfolder[3], 0.05 * canvas.width, 0.36 * canvas.height);
+    ctx.closePath();
+  }
+  function drawBar() {
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, 0.05 * canvas.height);
+    ctx.fillStyle = 'rgb(63,63,63)';
+    ctx.fill();
+    ctx.font = `${fontSize}px Arial`;
+    ctx.fillStyle = 'white';
+    ctx.fillText(wd.name, 0.05 * canvas.width, 0.035 * canvas.height);
+    ctx.closePath();
+  }
+  function drawCLI() {
+    ctx.beginPath();
+    ctx.rect(0, 0.75 * canvas.height, canvas.width, 0.25 * canvas.height);
+    ctx.fillStyle = '#000000';
     ctx.fill();
     ctx.closePath();
   }
-  // ending
-  ctx.font = '40px Arial';
-  ctx.fillText('🏠', 70, 600);
-
-  // detect collision
-  const left = Number(container.style.left.match(/\d+/)[0]);
-  const top = Number(container.style.top.match(/\d+/)[0]);
-  const collisionHandler = () => {
-    for (let dot of dots) {
-      if (!isReturning && document.elementFromPoint(left + dot[0], top + dot[1]) === div) {
-        div.textContent = '😭';
-        div.style = divStyle;
-        isReturning = true;
-        setTimeout(() => {
-          isReturning = false;
-          div.textContent = '😐';
-        }, 4000);
+  function drawGUI() {
+    ctx.beginPath();
+    ctx.rect(0.25 * canvas.width, 0.05 * canvas.height, 0.75 * canvas.width, 0.7 * canvas.height);
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fill();
+    ctx.closePath();
+    const fWidth = 0.2 * canvas.width;
+    const fHeight = 0.2 * canvas.width;
+    const toDisplay = Object.keys(wd.children).filter((f) => f[0] !== '.');
+    let fPosition = 1;
+    let gap = 65;
+    for (let f of toDisplay) {
+      ctx.beginPath();
+      let lineX = gap + fPosition * fWidth;
+      let lineY = 0.08 * canvas.height;
+      ctx.drawImage(firstFolder, lineX, lineY, fWidth, fHeight);
+      ctx.fill();
+      ctx.closePath();
+      ctx.font = `${fontSize}px Arial`;
+      ctx.fillStyle = '#000000';
+      ctx.fillText(f, gap + 55.5 - (f.length) * 3.3 + fPosition * fWidth, 0.3 * canvas.height);
+      ++fPosition;
+      gap += 20;
+    }
+  }
+  function drawText() {
+    ctx.font = `${fontSize}px Arial`;
+    ctx.fillStyle = 'white';
+    let linePosition = 1;
+    for (let i = Math.max(textArr.length - lineLimit, 0); i < textArr.length; ++i) {
+      for (let j = 0; j < textArr[i].length; j += lengthLimit) {
+        ctx.fillText(textArr[i].slice(j, j + lengthLimit), 0.02 * canvas.width, 0.77 * canvas.height + linePosition * fontSize);
+        ++linePosition;
       }
     }
-  };
-
-  // detect ending
-  const hitbox = [[70, 565], [110, 565], [110, 605], [70, 605]];
-  const temp = 4;
-  const border = getDots(hitbox, temp);
-  const endingHandler = () => {
-    for (let dot of border) {
-      if (!isReturning && document.elementFromPoint(left + dot[0], top + dot[1]) === div) {
-        div.textContent = '😎';
-      }
-    }
-  };
-
-  setInterval(() => {
-    collisionHandler();
-    endingHandler();
-  }, 10);
+  }
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = Number(gameContainer.style.width.match(/\d+/)[0]);
+    canvas.height = Number(gameContainer.style.height.match(/\d+/)[0]);
+    drawBackGround();
+    drawBar();
+    drawCLI();
+    drawGUI();
+    drawText();
+  }
+  setInterval(draw, 10);
 })();
