067113cd2e91611b2ba688f1c8cc05e7bafb451c
Candyroom136 <candyroom136@naver.com>
2021-08-22 18:10:53 +0900

    Modify pages/index.js ssg to ssr
    pages/index.js의 ssg를 ssr로 바꾸었을 때 웹사이트 작동여부를 확인하기 위해 코드변경

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home({ loginHandler, isLogin }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
   try {
     const { data } = await axios.get('http://localhost:3000/api/user', { headers: { cookie: `accessToken=${context.req.cookies.accessToken}` } });
     return {
