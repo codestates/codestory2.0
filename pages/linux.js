f52ea782fb507086b353b57cfbd867cf1bd0104e
Candyroom136 <candyroom136@naver.com>
2021-08-22 17:51:55 +0900

    Modify parameter of linux.js gss function

--- pages/linux.js
+++ pages/linux.js
@@ -67,7 +67,7 @@ export default function Linux({ loginHandler, isLogin, linuxGame }) {
   );
 };
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   const linuxDirectory = path.join(process.cwd(), '/games/linux/main.js');
   const linuxGame = await fs.readFile(linuxDirectory, 'utf8');
 
