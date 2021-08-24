dbedc76cf573df00280188470aaf6647ba312d90
Candyroom136 <candyroom136@naver.com>
2021-08-25 00:08:09 +0900

    Modify code for deploy test

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,6 +1,6 @@
 // const { generateAccessToken, sendAccessToken } = require('../../lib/json-token');
 const db = require('../../models');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
 export default async function signin(req, res) {
   switch (req.method) {
@@ -8,28 +8,29 @@ export default async function signin(req, res) {
     try {
       const password = req.body.password;
       const username = req.body.username;
+      res.status(200).json('hi');
       const userInfo = await db.users.findOne({
         where: { userId: username }
       });
-      if (!userInfo) {
-        res.status(400).json({ message: 'badrequest' });
-      } else {
-        const salt = userInfo.dataValues.salt;
-        crypto.randomBytes(64, (err, buf) => {
-          crypto.pbkdf2(password, salt, 98235, 64, 'sha512', async (err, key) => {
-            const incomingPassword = key.toString('base64');
-            if (incomingPassword === userInfo.dataValues.password) {
-              delete userInfo.dataValues.password;
-              delete userInfo.dataValues.salt;
-              res.status(200).json({ message: 'hi'});
-              // const accessToken = generateAccessToken(userInfo.dataValues);
-              // sendAccessToken(res, accessToken);
-            } else {
-              res.status(400).json({ message: 'badrequest' });
-            }
-          });
-        });
-      }
+      // if (!userInfo) {
+      //   res.status(400).json({ message: 'badrequest' });
+      // } else {
+      //   const salt = userInfo.dataValues.salt;
+      //   crypto.randomBytes(64, (err, buf) => {
+      //     crypto.pbkdf2(password, salt, 98235, 64, 'sha512', async (err, key) => {
+      //       const incomingPassword = key.toString('base64');
+      //       if (incomingPassword === userInfo.dataValues.password) {
+      //         delete userInfo.dataValues.password;
+      //         delete userInfo.dataValues.salt;
+      //         res.status(200).json({ message: 'hi'});
+      //         // const accessToken = generateAccessToken(userInfo.dataValues);
+      //         // sendAccessToken(res, accessToken);
+      //       } else {
+      //         res.status(400).json({ message: 'badrequest' });
+      //       }
+      //     });
+      //   });
+      // }
     }
     catch (error) {
       res.status(500).json({ message: 'Sorry Can\'t process your request' });
