aa09eb3e49b30c47104586d52c3b6ed82c27a436
JEONGBIN KIM <55874843+Candyroom136@users.noreply.github.com>
2021-08-26 15:20:48 +0900

    Modify code for deploy

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,10 @@ import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import axios from 'axios';
 import * as ga from '../lib/ga';
-import { Amplify, withSSRContext } from 'aws-amplify'; //배포 시 활성
-import awsExports from '../src/aws-exports';
+//import { Amplify, withSSRContext } from 'aws-amplify'; //배포 시 활성
+//import awsExports from '../src/aws-exports';
 
-Amplify.configure({ ...awsExports, ssr: true });
+//Amplify.configure({ ...awsExports, ssr: true });
 
 Router.events.on('routeChangeStart', () => NProgress.start()); 
 Router.events.on('routeChangeComplete', () => NProgress.done()); 
